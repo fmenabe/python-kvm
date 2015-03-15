d0c079a934dd3e01a419a67ab800e0d2c4bd546a
François Ménabé <francois.menabe@gmail.com>
2015-03-15 10:34:55 +0100

    Add an 'Image' object accessible via the 'image' property which allow to manage qemu images (create, resize, ...).

--- kvm.py
+++ kvm.py
@@ -311,6 +311,12 @@ def Hypervisor(host):
 
             return domains
 
+
+        @property
+        def image(self):
+            return _Image(weakref.ref(self)())
+
+
     return Hypervisor()
 
 
@@ -359,6 +365,72 @@ class _Domain(object):
         finally:
             signal.signal(signal.SIGALRM, old_handler)
             signal.alarm(0)
+
+
+class _Image(object):
+    def __init__(self, host):
+        self._host = host
+
+
+    def check(self, path, **kwargs):
+        return self._host.execute('qemu-img check', path, **kwargs)
+
+
+    def create(self, path, size, **kwargs):
+        return self._host.execute('qemu-img create', path, size, **kwargs)
+
+
+    def commit(self, path, **kwargs):
+        return self._host.execute('qemu-img commit', path, **kwargs)
+
+
+    def compare(self, *paths, **kwargs):
+        return self._host.execute('qemu-img compare', *paths, **kwargs)
+
+
+    def convert(self, src_path, dst_path, **kwargs):
+        with self._host.set_controls(options_place='after'):
+            return self._host.execute('qemu-img convert', src_path, dst_path, **kwargs)
+
+
+    def info(self, path, **kwargs):
+        status, stdout, stderr = self._host.execute('qemu-img info', path, **kwargs)
+        if not status:
+            raise OSError(stderr)
+        return _str_to_dict(stdout.splitlines())
+
+
+    def map(self, path, **kwargs):
+        return self._host.execute('qemu-img map', path, **kwargs)
+
+
+    def snapshot(self, path, **kwargs):
+        return self._host.execute('qemu-img snapshot', path, **kwargs)
+
+
+    def rebase(self, path, **kwargs):
+        return self._host.execute('qemu-img rebase', path, **kwargs)
+
+
+    def resize(self, path, size):
+        return self._host.execute('qemu-img resize', path, size)
+
+
+    def amend(self, path, **kwargs):
+        return self._host.execute('qemu-img amend', path, **kwargs)
+
+
+    def load(self, path, device='nbd0', **kwargs):
+        kwargs['c'] = '/dev/%s' % device
+        kwargs['d'] = False
+        return self._host.execute('qemu-nbd', path, **kwargs)
+
+
+    def unload(self, device='nbd0', **kwargs):
+        kwargs['c'] = False
+        kwargs['d'] = '/dev/%s' % device
+        return self._host.execute('qemu-nbd', **kwargs)
+
 
 for mname, mconf in _MAPPING['domain'].items():
     __add_method(_Domain, mname, mconf)
